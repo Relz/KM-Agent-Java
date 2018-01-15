64175ad695390d8484741133b6b01d4236d6e62d
Alexey Starovoitov <alexey.starovoitov@travelline.ru>
2018-01-15 20:03:41 +0300

    У перечислимого типа пассивного действия добавлен метод определения поворота по двум направлениям

--- src/main/java/ru/relz/km/model/action/PassiveAction.java
+++ src/main/java/ru/relz/km/model/action/PassiveAction.java
@@ -1,18 +1,32 @@
 package ru.relz.km.model.action;
 
+import ru.relz.km.model.agent.Direction;
+
 public enum PassiveAction {
 	NO_ACTION("noAct"),
-	TURN_AROUND("upSideDown"),
+	TURN_AROUND("upSideDn"),
 	TURN_LEFT("onLeft"),
 	TURN_RIGHT("onRight");
 
 	private String command;
-	private PassiveAction(String command) {
+	PassiveAction(String command) {
 		this.command = command;
 	}
 
 	@Override
 	public String toString() {
 		return command;
+	}
+
+	public static PassiveAction getRotateAction(Direction from, Direction to) {
+		if (Direction.getInversion(from) == to) {
+			return TURN_AROUND;
+		} else if (Direction.getLeft(from) == to) {
+			return TURN_LEFT;
+		} else if (Direction.getRight(from) == to) {
+			return TURN_RIGHT;
+		}
+
+		return NO_ACTION;
 	}
 }
