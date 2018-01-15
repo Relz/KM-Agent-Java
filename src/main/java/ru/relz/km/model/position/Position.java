f225c08b2ca3517b2eb5871cfa7e27272964660c
Starovoytov Alexey <relz0071@gmail.com>
2018-01-15 00:52:34 +0000

    Позиции можно установить лимит, можно получить список доступных вокруг позиций

--- src/main/java/ru/relz/km/model/position/Position.java
+++ src/main/java/ru/relz/km/model/position/Position.java
@@ -1,9 +1,14 @@
 package ru.relz.km.model.position;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Position implements PositionInterface {
+	private static PositionInterface limits;
+
 	public Position(int x, int y) {
-		this.x = x;
-		this.y = y;
+		this.x = limits == null ? x : Math.min(x, limits.getX());
+		this.y = limits == null ? y : Math.min(y, limits.getY());
 	}
 
 	private int x;
@@ -14,5 +19,48 @@ public class Position implements PositionInterface {
 	private int y;
 	public int getY() {
 		return y;
+	}
+
+	public List<PositionInterface> getAroundPositions() {
+		List<PositionInterface> result = new ArrayList<>();
+		if (y - 1 >= 0) {
+			result.add(new Position(x, y - 1));
+		}
+		if (limits == null || x + 1 <= limits.getX()) {
+			result.add(new Position(x + 1, y));
+		}
+		if (limits == null || y + 1 <= limits.getY()) {
+			result.add(new Position(x, y + 1));
+		}
+		if (x - 1 >= 0) {
+			result.add(new Position(x - 1, y));
+		}
+
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		PositionInterface other = (PositionInterface) obj;
+
+		return x == other.getX() && y == other.getY();
+	}
+
+	@Override
+	public int hashCode() {
+		return x + y * limits.getX() * limits.getY();
+	}
+
+	public static void setLimits(PositionInterface limits) {
+		Position.limits = limits;
 	}
 }
--- src/main/java/ru/relz/km/model/position/PositionInterface.java
+++ src/main/java/ru/relz/km/model/position/PositionInterface.java
@@ -1,6 +1,9 @@
 package ru.relz.km.model.position;
 
+import java.util.List;
+
 public interface PositionInterface {
 	int getX();
 	int getY();
+	List<PositionInterface> getAroundPositions();
 }
