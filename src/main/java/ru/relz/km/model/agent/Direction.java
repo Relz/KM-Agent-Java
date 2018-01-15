de8bd2ef9aa0fb62011af027e336cbcb0a8b8d3b
Starovoytov Alexey <relz0071@gmail.com>
2018-01-15 05:59:35 +0000

    Добавлены методы в Direction -получение инвертированного направления; -получение направления слева; -получение направления справа.

--- src/main/java/ru/relz/km/model/agent/Direction.java
+++ src/main/java/ru/relz/km/model/agent/Direction.java
@@ -20,11 +20,44 @@ public enum Direction {
 		put(LEFT.string, LEFT);
 	}};
 
+	private static Map<Direction, Direction> inversions = new HashMap<>(){{
+		put(UP, DOWN);
+		put(RIGHT, LEFT);
+		put(DOWN, UP);
+		put(LEFT, RIGHT);
+	}};
+
+	private static Map<Direction, Direction> lefts = new HashMap<>(){{
+		put(UP, LEFT);
+		put(LEFT, DOWN);
+		put(DOWN, RIGHT);
+		put(RIGHT, UP);
+	}};
+
+	private static Map<Direction, Direction> rights = new HashMap<>(){{
+		put(UP, RIGHT);
+		put(RIGHT, DOWN);
+		put(DOWN, LEFT);
+		put(LEFT, UP);
+	}};
+
 	public String getString() {
 		return string;
 	}
 
 	public static Direction createFromString(String string) {
 		return stringToDirection.get(string);
+	}
+
+	public static Direction getInversion(Direction direction) {
+		return inversions.get(direction);
+	}
+
+	public static Direction getLeft(Direction direction) {
+		return lefts.get(direction);
+	}
+
+	public static Direction getRight(Direction direction) {
+		return rights.get(direction);
 	}
 }
